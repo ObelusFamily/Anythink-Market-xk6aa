b006acb561316b280c86e6111ce60c0059c4e37f
David O'Trakoun <me@davidosomething.com>
2022-09-22 15:22:15 -0400

    hide search until click get text

--- frontend/src/components/Home/Banner.js
+++ frontend/src/components/Home/Banner.js
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../../imgs/logo.png";
 
 const Banner = ({ onChangeTitle, page, pager }) => {
+  const [showSearch, setShowSearch] = useState(false);
+
   const handleChange = (e) => {
     const title = e.target.value;
     if (title?.length === 0) {
@@ -18,12 +20,17 @@ const Banner = ({ onChangeTitle, page, pager }) => {
       <div className="container p-4 text-center">
         <img src={logo} alt="banner" />
         <div>
-          <span id="get-part">A place to get</span>
+          <span id="get-part" onClick={() => setShowSearch(true)}>
+            A place to get
+          </span>
           <input
             id="search-box"
             type="search"
             placeholder="What is it that you truly desire?"
             onChange={handleChange}
+            style={{
+              display: showSearch ? "inline" : "none",
+            }}
           />
           <span> the cool stuff.</span>
         </div>
